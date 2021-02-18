84fffdd5ffb48a904d14d053e1a55551f6fc00e7
Mario Ricaurte <m.ricaurte@uniandes.edu.co>
2021-02-18 09:16:50 -0500

    Entrega Ejemplo – laboratorio 4

--- App/model.py
+++ App/model.py
@@ -188,7 +188,7 @@ def compareauthors(authorname1, author):
 
 
 def compareratings(book1, book2):
-    return (float(book1['average_rating']) > float(book2['average_rating']))
+    return (float(book1['average_rating']) < float(book2['average_rating']))
 
 
 def comparetagnames(name, tag):
@@ -204,4 +204,4 @@ def sortBooks(catalog, size):
     sa.sort(sub_list, compareratings)
     stop_time = time.process_time()
     elapsed_time_mseg = (stop_time - start_time)*1000
-    return elapsed_time_mseg
+    return elapsed_time_mseg, sub_list
